06fc22b24fd7baac9e16fc2f66741138f81457db
Alefsandler Felisberto <aleffelisberto@gmail.com>
2021-09-20 12:55:20 -0300

    Finish the project and details

--- script.js
+++ script.js
@@ -9,33 +9,56 @@ const rate = document.getElementById('rate')
 // Object to handle API requests and responses
 const exchangeRateApiInfo = {
     accessKey: '7e063b53977016d87f39a65e',
-    fetchCurrencies: function(baseCurrency) {
-        fetch(`https://v6.exchangerate-api.com/v6/${this.accessKey}/latest/USD`)
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                throw new Error('No currency found')
-            }
-        })
-        .then(data => this.createCurrencyOptions(data))
-        .catch(err => {
-            throw err
-        })
+    currencyValues: {},
+    fetchCurrencies: async function(baseCurrency = 'USD') {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${this.accessKey}/latest/${baseCurrency}`)
+        const data = await response.json()
+        this.currencyValues = data.conversion_rates
     },
-    createCurrencyOptions: function(data) {
-        const {conversion_rates} = data
-        const currencies = Object.keys(conversion_rates)
-        
-        currencies.forEach(currency => {
-            const currencyOption1 = document.createElement('option')
-            const currencyOption2 = document.createElement('option')
-            currencyOption1.value = currencyOption2.value = currency
-            currencyOption1.innerText = currencyOption2.innerText = currency
-            initialCurrency.add(currencyOption1)
-            finalCurrency.add(currencyOption2)
-        })
-    }
 }
 
-exchangeRateApiInfo.fetchCurrencies('USD')
+function createCurrencyOptions() {
+    const currencies = Object.keys(exchangeRateApiInfo.currencyValues)
+    
+    currencies.forEach(currency => {
+        const currencyOption1 = document.createElement('option')
+        const currencyOption2 = document.createElement('option')
+        currencyOption1.value = currencyOption2.value = currency
+        currencyOption1.innerText = currencyOption2.innerText = currency
+        initialCurrency.add(currencyOption1)
+        finalCurrency.add(currencyOption2)
+    })
+
+    finalCurrency.value = 'EUR'
+}
+
+async function calcExchangeRate() {
+    await exchangeRateApiInfo.fetchCurrencies(initialCurrency.value)
+    const currencyValue2 = Number(initialCurrencyValue.value) * exchangeRateApiInfo.currencyValues[finalCurrency.value]
+
+    const conversionRate = `${initialCurrencyValue.value} ${initialCurrency.value} = ${currencyValue2} ${finalCurrency.value}`
+    rate.innerText = conversionRate
+    finalCurrencyValue.innerText = currencyValue2
+}
+
+async function swapCurrencies() {
+    const temp = initialCurrency.value
+    initialCurrency.value = finalCurrency.value
+    finalCurrency.value = temp
+    calcExchangeRate()
+}
+
+async function main() {
+    await exchangeRateApiInfo.fetchCurrencies()
+
+    createCurrencyOptions()
+    calcExchangeRate()
+
+    // Events
+    initialCurrency.addEventListener('change', calcExchangeRate)
+    finalCurrency.addEventListener('change', calcExchangeRate)
+    initialCurrencyValue.addEventListener('input', calcExchangeRate)
+    swapButton.addEventListener('click', swapCurrencies)
+}
+
+main()
